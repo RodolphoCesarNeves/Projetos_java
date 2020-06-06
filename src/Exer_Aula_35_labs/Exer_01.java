5702e78bd2ea70c75bf112d5b41234782a79fc16
Rodolpho <rodolphocrn@gmail.com>
2020-06-06 15:45:17 -0300

    Recursividade

---
+++ src/Exer_Aula_35_labs/Exer_01.java
@@ -0,0 +1,12 @@
+package Exer_Aula_35_labs;
+
+public class Exer_01 {
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i<10;i++) {
+		System.out.print(Calculadora.fibonacci(i)+ " ");
+		}
+	}
+
+}
---
+++ src/Exer_Aula_35_labs/Exer_02.java
@@ -0,0 +1,11 @@
+package Exer_Aula_35_labs;
+
+public class Exer_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.somatorio(5));
+
+	}
+
+}
